ccb82b8bc4f1cd325a8be0a1f85b20d123a953ee
BrockHanni <hannibr26@gmail.com>
2023-08-01 20:09:16 -0600

    header & footer ready for styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import footer from './components/Footer.js';
-import header from './components/Header.js';
-import home from './components/Home.js';
-import products from './components/Products.js';
-import review from './components/Review.js';
-import signin from './components/Signin.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+// import home from './components/Home.js';
+// import products from './components/Products.js';
+// import review from './components/Review.js';
+// import signin from './components/Signin.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className=''>
+      <Header />
+      <Footer />
+    </main>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">Please call for prices/questions</span>
+            </div>
+            <div className="container">
+                <h1>JHMedical, Inc.</h1>
+                <h1>3005 Cave Hollow Way</h1>
+                <h1>Bountiful, UT 84010</h1>
+                <br></br>
+                <br></br>
+                <h1>1-801-540-9158</h1>
+            </div>
+            <div>
+                {/* links to calcoast? */}
+            </div>
+
+        </footer>
+    )
+}
