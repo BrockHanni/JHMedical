f10f15ee71376f17c1954d86b29c1440ef97460d
BrockHanni <hannibr26@gmail.com>
2023-08-03 18:55:32 -0600

    updated logins

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Header from '../client/src/components/components/Header.js';
-import Home from '../client/src/components/components/Home.js';
-import Products from '../client/src/components/components/Products.js';
-import Signin from '../client/src/components/components/Signin.js';
-import Review from '../client/src/components/components/Review.js';
-import Footer from '../client/src/components/components/Footer.js';
+import Header from '../client/src/components/Header.js';
+import Home from '../client/src/components/Home.js';
+import Products from '../client/src/components/Products.js';
+import Signin from '../client/src/components/Signin.js';
+import Review from '../client/src/components/Review.js';
+import Footer from '../client/src/components/Footer.js';
 
 // import home from './components/Home.js';
 // import products from './components/Products.js';
