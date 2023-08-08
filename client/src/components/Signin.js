896dd2b1fb8ad2a3bcae5a2b74712a89b061c291
Blake H <blake.hollingsworth80@gmail.com>
2023-08-07 21:28:24 -0600

    added jwt-decode for the signin page

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
 
 export default function Signin() {
-    return(
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordPlaceholder, setPlaceholder] = useState("")
+
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { username: username, password: password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    function handleInputChange(e) {
+        const { target } = e
+        const inputType = target.name
+        const inputValue = target.value
+
+        // sync the state and the value in the dom
+        if (inputType === "username") {
+            setUsername(inputValue)
+        } else if (inputType === "password") {
+            setPassword(passwordPlaceholder + "*")
+        }
+    };
+
+    return (
         <div>
             <h1>Sign In</h1>
-            <section>
+            <form>
                 <label>
                     Username:
-                    <input type="text" name="Username" />
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <label>
                     Password:
-                    <input type="text" name="Password" />
+                    <input
+                        type="text"
+                        name="Password"
+                        value={passwordPlaceholder}
+                        onChange={handleInputChange}
+                    />
                 </label>
-                <button>Sign In</button>
-            </section>
+                <button type="button" onClick={handleFormSubmit}>Sign In</button>
+            </form>
         </div>
     )
 }
