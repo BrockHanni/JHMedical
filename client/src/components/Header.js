4806924b56e748ac99ba2c71e629483423de3bb5
Blake H <blake.hollingsworth80@gmail.com>
2023-08-08 20:17:59 -0600

    added logged auth to header links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import jhmedical from '../assets/images/jhmedical.jpg';
+import Auth from '../utils/auth';
 
 export default function Header() {
+
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
     return (
-        <header className="header">
-            <div className="header-container">
+        <header className="header-container">
+            <div>
                 <img src={jhmedical} alt="Logo " className="logo">
 
                 </img>
@@ -16,15 +23,22 @@ export default function Header() {
             <div>
                 <Navbar expand="lg" className="bg-body-tertiary">
                     <Container>
-                        <h1>JHMedical</h1>
-                        <Nav className="me-auto">
+                        <Nav className="nav-link">
                             <Nav.Link href="./"> Home </Nav.Link>
 
-                            <Nav.Link href="./Products"> Products </Nav.Link>
+                            {Auth.loggedIn() ? (
+                                <Nav.Link href="./ProductsLogin"> Products </Nav.Link>
+                            ) : (
+
+                                <Nav.Link href="./Products"> Products </Nav.Link>
+                            )}
 
                             <Nav.Link href="./Review"> Review </Nav.Link>
-
-                            <Nav.Link href="./Signin"> Sign In </Nav.Link>
+                            {Auth.loggedIn() ? (
+                                <Nav.Link onClick={logout}> Sign Out </Nav.Link>
+                            ) : (
+                                <Nav.Link href="./Signin"> Sign In </Nav.Link>
+                            )}
                         </Nav>
                     </Container>
                 </Navbar>
