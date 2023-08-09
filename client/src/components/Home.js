66946ea47e92999ff77c2eb8bff2ffc457516660
BrockHanni <hannibr26@gmail.com>
2023-08-08 21:05:32 -0600

    Home page styled

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Family from "../assets/images/FamPicture.jpg";
 
 export default function Home() {
     return (
         <div>
-            <section>
+            <section className="home-container">
                 <h1>About Us</h1>
                 <p> JHMedical was founded in 2002 by Jason Hanni, with the idea of providing excellent service for both customers and suppliers in Ophthalmic equipment.  Initially, JH Medical was founded to represent mid-size manufacturers in the Latin American market.  We handle all business functions for these manufacturers, including registration assistance, sales, service, and support for those manufacturers throughout Latin America.  At the same time, JH Medical began offering the same service and support to local Ophthalmologists and Optometrists in Utah.  As the industry has changed, JH Medical has focused much more intently on the local market in Utah and added service in the surrounding states of Colorado, Wyoming, Idaho, and Montana.  In 2018, JH Medical combined forces with Cal Coast Ophthalmic Instruments, a major distributor of ophthalmic equipment based in Southern California, to expand operations into the Mountain States area.  Cal Coast Ophthalmic Instruments distributes nearly all of the major brands in the industry.  We offer everything from traditional lane equipment like chairs, stands, slit lamps and phoropters, to high tech equipment such as OCT, Visual Field, Digital Retinal Cameras, the Auto phoropters.  Since that time, we have worked tirelessly to share our experience, expertise, and services to eye care professionals throughout the area with the highest quality new and used equipment, as well as maintaining that equipment to ensure that it will continue to function properly for many years into the future. As we begin our third decade of service, we look forward to a continued partnership with both our suppliers and customers.  We promise to offer the highest quality products and services available in the industry with our personal guarantee that we will do everything possible to ensure your satisfaction.</p>
+                <br />
+                <img src={Family} className="family-picture"></img>
             </section>
         </div>
     )
